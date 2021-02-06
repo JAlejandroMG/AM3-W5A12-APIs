0ace6aea16fbab1cb9f10ffbf6acf9d9c98eb1ec
JAlejandroMG <jalejandromg@tutanota.com>
2021-02-06 14:46:14 -0600

    Authentication's test passed

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,15 +1,32 @@
 import { Users } from "../models/";
 import bcryptjs from 'bcryptjs';
+import { generateJWT } from '../middlewares/jwt';
 
-//1. Completar la logica para manejar el inicio de sesión
+
+//{1. Completar la logica para manejar el inicio de sesión
 // - responder con un codigo de estado 401 cuando las credenciales sean incorrectas
 // - responder con un mensaje (message) y codigo de estado 200 cuando las credenciales sean correctas
 // - responder con el token jwt (token) 
 export const login = async (req, res) => {
-
+   const { email, password } = req.body;
+   const results = await Users.findOne({where: {email}});
+   if(results) {
+      const valid = bcryptjs.compareSync(password, results.password);
+      if(valid){
+         const token = generateJWT(results);
+         return res.status(200).json({
+            message: "Has iniciado sesión exitosamente",
+            token
+         })
+      }
+      return res.status(401).json({message: "El usuario y/o la contraseña son incorrectas"})
+   }
+   return res.status(401).json({message: "El usuario y/o la contraseña son incorrectas"})
 }
 
-//2. Completar el registro de usuario
+
+
+//{2. Completar el registro de usuario
 // - responder con un codigo de estado fallido 400 > cuando hagan falta campos o cuando el usuario ya exista en la base de datos
 // - responder con el objeto del usuario que ha sido creado y un codigo 201 cuando el registro sea satisfactorio
 export const signIn = async (req, res) => {
@@ -25,7 +42,7 @@ export const signIn = async (req, res) => {
          const encryptedPassword = bcryptjs.hashSync(password, 10);
          req.body.password = encryptedPassword;
 
-         const [user, created] = await Users.findOrCreate({
+         let [user, created] = await Users.findOrCreate({
             where: { email: email },
             defaults: req.body
          });
@@ -36,6 +53,12 @@ export const signIn = async (req, res) => {
          } else{
 
             //* El usuario ha sido registrado exitosamente
+            user= {
+               id: user.id,
+               firstName: user.firstName,
+               lastName: user.lastName,
+               email: user.email
+            }
             return res.status(201).json(user);
          }
       }
--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
+app.get('/', (req, res) => {
+    res.send("Bienvenido a la creación de APIs")
+});
+
 app.listen(PORT, () => {
     console.log("Servidor escuchando sobre el puerto", PORT);
 });
